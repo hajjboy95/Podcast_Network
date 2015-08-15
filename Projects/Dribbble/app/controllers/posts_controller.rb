e9dcbd9775e68a5433a8d2525ccd4b78ae1d9b75
hajjboy95 <gizmo-boy95@hotmail.com>
2015-08-14 23:27:31 -0700

    Add and setup voting

--- Projects/Dribbble/app/controllers/posts_controller.rb
+++ Projects/Dribbble/app/controllers/posts_controller.rb
@@ -2,8 +2,10 @@
 
 class PostsController < ApplicationController
 
-  before_action :find_post , only: [:show , :edit , :update , :destroy]
+  before_action :find_post , only: [:show , :edit , :update , :destroy, :upvote , :downvote]
   before_action :authenticate_user! , only: [:edit , :create , :update, :destroy]
+
+
   def index
     @posts = Post.all.order("created_at DESC")
   end
@@ -47,6 +49,17 @@ class PostsController < ApplicationController
     @post.destroy
     redirect_to root_path
 
+  end
+
+
+  def upvote
+    @post.upvote_by current_user
+    redirect_to :back
+  end
+
+  def downvote
+    @post.downvote_by current_user
+    redirect_to :back
   end
 
   private
